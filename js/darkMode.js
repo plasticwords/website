666ef68a84020cdcc55241102ec4ec425340d03f
timothyrobinsonblog <53365029+timothyrobinsonblog@users.noreply.github.com>
2023-07-15 14:51:39 +1000

    clean darkMode.js

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,8 +1,5 @@
-// DARK MODE TO DO:
-// read through JS to ensure it's accurate, dark == light
-// clean and carry on with rest of site
-// ? Keep constant across pages when move through site.
-// ? Simplify in chatGPT
+// ? Simplify in chatGPT && Keep dark mode activated when move through site, this line of code may help:
+// if (darkButton.classList.contains('active'))
 
 // implement dark mode:
 const darkButton = document.querySelector('.dark-mode');
@@ -32,24 +29,20 @@ function enableDarkMode() {
       icon.style.color = '#7d8590'; // light silver
     });
   }
-// });
 
 darkButton.addEventListener('click', enableDarkMode);
 darkButtonResponsive.addEventListener('click', enableDarkMode);
 
-// dark mode instructions display::
-darkButton.addEventListener('mouseover', () => {  
+// display dark mode instructions:
+darkButton.addEventListener('mouseover', () => {
   darkModeInstruct.style.display = 'inline-block';
 });
 
-darkButton.addEventListener('mouseout', () => {  
+darkButton.addEventListener('mouseout', () => { 
   darkModeInstruct.style.display = 'none';
 });
 
-// if (darkButton.classList.contains('active'))
-// if (lightButton.classList.contains('active'))
-
-// implement light mode
+// implement light mode:
 const lightButton = document.querySelector('.light-mode');
 const lightButtonResponsive = document.querySelector('.light-mode-responsive');
 const lightBackground = document.querySelector('html');
@@ -77,12 +70,11 @@ function enableLightMode() {
       icon.style.color = '#7C7C81';
     });
   }
-// });
 
 lightButton.addEventListener('click', enableLightMode);
 lightButtonResponsive.addEventListener('click', enableLightMode);
 
-// light mode instructions display:
+// display light mode instructions:
 lightButton.addEventListener('mouseover', () => {  
   lightModeInstruct.style.display = 'inline-block';
 });
@@ -90,64 +82,3 @@ lightButton.addEventListener('mouseover', () => {
 lightButton.addEventListener('mouseout', () => {  
   lightModeInstruct.style.display = 'none';
 });
-
-// ---------------------------------------
-// UNUSED MENU JS:
-// const darkMenu = document.querySelector('.menu');
-// const darkActivePage = document.querySelector('#active-page');
-// const darkMenuText = document.querySelectorAll('.menu a');
-
-// darkMenu.style.color = 'grey';
-// darkMenu.style.opacity = '0.75';
-// darkActivePage.style.color = '#B450B0';
-// darkActivePage.style.opacity = '1';
-
-// darkMenuText.forEach(menu => {
-//     menu.style.color = 'grey';
-//     menu.style.opacity = '0.75';
-//   });
-
-// ---------------------------------------
-// UNUSED BURGER MENU (DARK / LIGHT)
-
-// Dark:
-// const darkResponsiveBurger = document.querySelectorAll('.burger div');
-// const darkResponsiveBurgerLine1 = document.querySelector('.line1');
-// const darkResponsiveBurgerLine2 = document.querySelector('.line2');
-// const darkResponsiveBurgerLine3 = document.querySelector('.line3');
-
-  // darkResponsiveBurgerLine1.style.background = '#7d8590';
-  // darkResponsiveBurgerLine1.style.border = '0.5px solid #7d8590';
-  // darkResponsiveBurgerLine2.style.background = '#7d8590';
-  // darkResponsiveBurgerLine2.style.border = '0.5px solid #7d8590';
-  // darkResponsiveBurgerLine3.style.background = '#7d8590';
-  // darkResponsiveBurgerLine3.style.border = '0.5px solid #7d8590';
-
-// Light:
-// const lightResponsiveBurger = document.querySelectorAll('.burger div');
-
-// lightResponsiveBurger.forEach(burgerDiv => {
-//   burgerDiv.style.background = 'lightgray';
-//   burgerDiv.style.border = '1px solid gray';
-// });
-
-// ---------------------------------------
-// UNUSED LOGO CHANGE (DARK / LIGHT)
-
-// Dark:
-// const lightLogoHide = document.querySelector('.light-logo');
-// const darkLogoShow = document.querySelector('.dark-logo');
-// const darkGradientText = document.querySelector('.gradient-text');
-
-// lightLogoHide.style.display = 'none'; 
-// darkLogoShow.style.display = 'block';
-// darkGradientText.style.color = '#7d8590'; // light silver
-
-// Light:
-// const lightLogoShow = document.querySelector('.light-logo');
-// const darkLogoHide = document.querySelector('.dark-logo');
-// const lightGradientText = document.querySelector('.gradient-text');
-
-// lightLogoShow.style.display = 'block'; 
-// darkLogoHide.style.display = 'none';
-// lightGradientText.style.color = '#B450B0';
